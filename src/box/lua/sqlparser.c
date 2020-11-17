448a15f431626b5123d8703f8a4f6da477f97065
Timur Safin <tsafin@tarantool.org>
2020-11-17 23:28:59 +0300

    sql - added sqlparser.unparse for completeness
    Added sqlparser.unparse to dispose results of
    sqlparser.parse call.

--- src/box/lua/sqlparser.c
+++ src/box/lua/sqlparser.c
@@ -54,6 +54,13 @@ error:
 }
 
 static int
+lbox_sqlparser_unparse(struct lua_State *L)
+{
+	lua_pushliteral(L, "sqlparser.unparse");
+	return 1;
+}
+
+static int
 lbox_sqlparser_serialize(struct lua_State *L)
 {
 	lua_pushliteral(L, "sqlparser.serialize");
@@ -72,6 +79,7 @@ box_lua_sqlparser_init(struct lua_State *L)
 {
 	static const struct luaL_Reg meta[] = {
 		{ "parse", lbox_sqlparser_parse },
+		{ "unparse", lbox_sqlparser_unparse },
 		{ "serialize", lbox_sqlparser_serialize },
 		{ "deserialize", lbox_sqlparser_deserialize },
 		{ NULL, NULL },
