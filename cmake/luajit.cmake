0530261ebf36036919d2e19bb7b7a81896a0477c
Sergey Bronnikov <sergeyb@tarantool.org>
2020-12-11 22:34:39 +0300

    cmake: pass an external LDFLAGS to LuaJIT

--- cmake/luajit.cmake
+++ cmake/luajit.cmake
@@ -245,6 +245,7 @@ macro(luajit_build)
     set (luajit_buildoptions
         BUILDMODE=static
         HOST_CC="${luajit_hostcc}"
+        LDFLAGS="${luajit_ldflags}"
         TARGET_CC="${luajit_cc}"
         TARGET_CFLAGS="${luajit_cflags}"
         TARGET_LD="${luajit_ld}"
