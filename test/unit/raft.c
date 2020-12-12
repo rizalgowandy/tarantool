3134790e5995603c74baf7ef14b2451e7225d703
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2020-12-12 16:11:11 +0100

    fix too big state

--- test/unit/raft.c
+++ test/unit/raft.c
@@ -248,7 +248,7 @@ raft_test_recovery(void)
 static void
 raft_test_bad_msg(void)
 {
-	raft_start_test(7);
+	raft_start_test(9);
 	struct raft_msg msg;
 	struct raft_node node;
 	struct vclock vclock;
@@ -285,6 +285,14 @@ raft_test_bad_msg(void)
 	};
 	is(raft_node_process_msg(&node, &msg, 2), -1, "node can't be a "
 	   "candidate without vclock");
+	is(node.raft.term, 1, "term from the bad message wasn't used");
+
+	msg = (struct raft_msg){
+		.state = 10000,
+		.term = 2,
+		.vote = 2,
+	};
+	is(raft_node_process_msg(&node, &msg, 2), -1, "bad state");
 	is(node.raft.term, 1, "term from the bad message wasn't used");
 
 	raft_node_destroy(&node);
